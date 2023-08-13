5f5677cdd44de007063510e4bde65a96cf3246be
Hafsa Hussein <hafsahussein2014@gmail.com>
2023-08-13 16:18:06 -0500

    html and incorporating functions on JS

---
+++ client.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+
+let salaryArray = []; //Way to store salaries
+                        
+let monthlyCost = 0; // Initialize monthly cost
+
+function submitForm(event) {
+    console.log('submitForm');
+    event.preventDefault();
+}
+
+function submit(event) {
+    event.preventDefault();
+
+
+let firstName = document.querySelector('#FirstName-input').value;
+let lastName = document.querySelector('#LastName-input').value;
+let iDNumber = Number(document.querySelector('#IdNumber-input').value);
+let jobTitle= document.querySelector('#JobTitle-input').value;
+let annualSalary = Number(document.querySelector('#AnnualSalary-input').value);
+    
+    
+}
+
