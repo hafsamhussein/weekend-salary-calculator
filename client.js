592028d62a1359fa2a51bbd744146eabf693e4c6
Hafsa Hussein <hafsahussein2014@gmail.com>
2023-08-13 17:35:44 -0500

    added some style to css

--- client.js
+++ client.js
@@ -21,11 +21,9 @@ let annualSalary = Number(document.querySelector('#AnnualSalary-input').value);
     
     
 }
-let monthlyTotal = annualSalary / 12;
-monthlyCost += monthlyTotal;
 
 let monthlyColorElement = document.querySelector("#monthlyTotal");
-if (monthlyCost > 20000) {
-    monthlyColorElement.style.backgroundColor = "red";
+if(monthlyCost > 20000) {
+  monthlyColorElement.style.backgroundColor = "red";
 }
 monthlyColorElement.innerText = monthlyCost;
